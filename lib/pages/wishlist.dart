ce50d9638de6871304ed3b2e1450cb8107084fc7
unknown <osamadrbas31@gmail.com>
2024-11-25 14:27:15 +0200

    added cart and home and user and wishlist page

--- lib/pages/wishlist.dart
+++ lib/pages/wishlist.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class WishlistPage extends StatelessWidget {
+  const WishlistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Cart Pfffffffffffffffage");
+    return const Text("wishlist page");
   }
 }
