e15289aea9907db353ba182ad0d797f5e258c511
unknown <osamadrbas31@gmail.com>
2024-11-25 16:13:10 +0200

    created icons_header widget for home page

---
+++ lib/widgets/home/icons_header.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IconsHeader extends StatelessWidget {
+  const IconsHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Icons Header");
+  }
+}
